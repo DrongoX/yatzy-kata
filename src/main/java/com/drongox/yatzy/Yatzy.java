377053108fd0af010cc14aede2be20410d3b0ddb
DrongoX <drongox@gmail.com>
2019-07-13 15:50:59 +0200

    refactor Yatzy: small straight strategy

--- src/main/java/com/drongox/yatzy/Yatzy.java
+++ src/main/java/com/drongox/yatzy/Yatzy.java
@@ -145,20 +145,16 @@ public class Yatzy
 
   public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
   {
-    int[] tallies;
-    tallies = new int[6];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[0] == 1 &&
-        tallies[1] == 1 &&
-        tallies[2] == 1 &&
-        tallies[3] == 1 &&
-        tallies[4] == 1)
+    Set<Integer> smallStraight = Set.of(1, 2, 3, 4, 5);
+    boolean isMatching = IntStream.of(d1, d2, d3, d4, d5)
+                                  .boxed()
+                                  .collect(toSet())
+                                  .equals(smallStraight);
+    if (isMatching) {
       return 15;
-    return 0;
+    } else {
+      return 0;
+    }
   }
 
 
