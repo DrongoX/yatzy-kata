b903f679a76a1890ec98e925291dcabc921d4785
DrongoX <drongox@gmail.com>
2019-07-13 15:10:33 +0200

    refactor Yatzy: pair strategy

--- src/main/java/com/drongox/yatzy/Yatzy.java
+++ src/main/java/com/drongox/yatzy/Yatzy.java
@@ -1,6 +1,13 @@
 package com.drongox.yatzy;
 
+import java.util.Map;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Yatzy
 {
@@ -83,17 +90,15 @@ public class Yatzy
 
   public static int score_pair(int d1, int d2, int d3, int d4, int d5)
   {
-    int[] counts = new int[6];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    int at;
-    for (at = 0; at != 6; at++)
-      if (counts[6 - at - 1] >= 2)
-        return (6 - at) * 2;
-    return 0;
+    Map<Integer, Long> counts = Stream.of(d1, d2, d3, d4, d5)
+                                      .collect(groupingBy(identity(), counting()));
+    return counts.entrySet()
+                 .stream()
+                 .filter(entry -> entry.getValue() >= 2)
+                 .max(comparingByKey())
+                 .map(Map.Entry::getKey)
+                 .map(die -> die * 2)
+                 .orElse(0);
   }
 
 
