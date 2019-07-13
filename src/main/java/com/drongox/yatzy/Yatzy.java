6a5d6e85c0e89eebf05bca2c34211a11ef3f7331
DrongoX <drongox@gmail.com>
2019-07-13 16:39:57 +0200

    refactoring: drop unused methods

--- src/main/java/com/drongox/yatzy/Yatzy.java
+++ src/main/java/com/drongox/yatzy/Yatzy.java
@@ -19,30 +19,12 @@ public class Yatzy
   }
 
 
-  public static int twos(int d1, int d2, int d3, int d4, int d5)
-  {
-    return exactNumberStrategy(2, d1, d2, d3, d4, d5);
-  }
-
-
-  public static int threes(int d1, int d2, int d3, int d4, int d5)
-  {
-    return exactNumberStrategy(3, d1, d2, d3, d4, d5);
-  }
-
-
   protected int[] dice;
 
 
   public Yatzy(int d1, int d2, int d3, int d4, int d5)
   {
     dice = new int[]{d1, d2, d3, d4, d5};
-  }
-
-
-  public int fours()
-  {
-    return exactNumberStrategy(4, dice);
   }
 
 
